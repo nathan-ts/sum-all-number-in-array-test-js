4a1f3acccc24b1e88d533b6eb3a17b950a0b7bdc
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-11 21:40:41 +0000

    done recursive deep sum

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let first = array[0];
+  if (Array.isArray(first)) first = sumItems(first);
+  if (array.length === 0) return 0;
+  else if (array.length === 1) return first;
+  else return first + sumItems(array.slice(1));
 }
 
 module.exports = sumItems;
+
+console.log(sumItems([0,[1,2],[3,[4]],5])); // 15
+console.log(sumItems([[1, 2, [[3], 4]], 5, []])); // 15
+console.log(sumItems([[[[[[[[[[[[[1]]]]]],2]]],3]],4,5]])); // 15
+
+
+/*
+base case:  array length 1, and array[0] not array
+            
+recursive case: array length 1, and array[0] is array
+                array length > 1, 
+
+
+
+
+
+*/
